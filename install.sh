99ca7f8fc2f4c8440c195ae741afd3d833ac5a83
Imon Roy <royimonroy@gmail.com>
2023-06-10 01:56:00 +0530

    removed makefile, enhanced script

--- install.sh
+++ install.sh
@@ -1,29 +1,120 @@
 #!/bin/bash
+echo ""
+echo "___                       _       ____                           "
+echo "|_ _|_ __ ___   ___  _ __ ( )___  / ___|  ___ _ ____   _____ _ __ "
+echo " | || '_ \` _ \ / _ \| '_ \|// __| \___ \ / _ \ '__\ \ / / _ \ '__|"
+echo " | || | | | | | (_) | | | | \__ \  ___) |  __/ |   \ V /  __/ |   "
+echo "|___|_| |_| |_|\___/|_| |_| |___/ |____/ \___|_|    \_/ \___|_|   "
+echo ""
+echo " ____             __ _       "
+echo "/ ___|___  _ __  / _(_) __ _ "
+echo "| |   / _ \| '_ \| |_| |/ _  |"
+echo "| |__| (_) | | | |  _| | (_| |"
+echo " \____\___/|_| |_|_| |_|\__, |"
+echo "                        |___/ "
+echo ""
 
-# Check if make is installed
-if ! command -v make &> /dev/null
+echo "This script will install docker, docker-compose, git and apache2-utils"
+
+echo "Traefik + Portainer for easy access DevOps"
+
+# Check if docker is installed
+if ! command -v docker &> /dev/null
 then
-    # Install make
+    # Install docker
     if command -v apt-get &> /dev/null
     then
         # Debian/Ubuntu
-        sudo apt-get update
-        sudo apt-get install make
+        sudo apt-get install docker
     elif command -v yum &> /dev/null
     then
         # CentOS/Fedora
-        sudo yum install make
+        sudo yum install docker
     elif command -v brew &> /dev/null
     then
         # macOS (using Homebrew)
-        brew install make
+        brew install docker
     else
-        echo "Error: make is not installed and package manager not found"
+        echo "Error: docker is not installed and package manager not found"
         exit 1
     fi
 else
-    echo "make is already installed"
+    echo "docker is already installed"
 fi
+
+# Check if docker-compose is installed
+if ! command -v docker-compose &> /dev/null
+then
+    # Install docker-compose
+    if command -v apt-get &> /dev/null
+    then
+        # Debian/Ubuntu
+        sudo apt-get install docker-compose
+    elif command -v yum &> /dev/null
+    then
+        # CentOS/Fedora
+        sudo yum install docker-compose
+    elif command -v brew &> /dev/null
+    then
+        # macOS (using Homebrew)
+        brew install docker-compose
+    else
+        echo "Error: docker-compose is not installed and package manager not found"
+        exit 1
+    fi
+else
+    echo "docker-compose is already installed"
+fi
+
+# Check if git is installed
+if ! command -v git &> /dev/null
+then
+    # Install git
+    if command -v apt-get &> /dev/null
+    then
+        # Debian/Ubuntu
+        sudo apt-get install git
+    elif command -v yum &> /dev/null
+    then
+        # CentOS/Fedora
+        sudo yum install git
+    elif command -v brew &> /dev/null
+    then
+        # macOS (using Homebrew)
+        brew install git
+    else
+        echo "Error: git is not installed and package manager not found"
+        exit 1
+    fi
+else
+    echo "git is already installed"
+fi
+
+# Check if htpasswd is installed
+if ! command -v htpasswd &> /dev/null
+then
+    # Install htpasswd
+    if command -v apt-get &> /dev/null
+    then
+        # Debian/Ubuntu
+        sudo apt-get install apache2-utils
+    elif command -v yum &> /dev/null
+    then
+        # CentOS/Fedora
+        sudo yum install httpd-tools
+    elif command -v brew &> /dev/null
+    then
+        # macOS (using Homebrew)
+        brew install httpd-tools
+    else
+        echo "Error: htpasswd is not installed and package manager not found"
+        exit 1
+    fi
+else
+    echo "htpasswd is already installed"
+fi
+
+
 
 # Clone the repository
 git clone https://github.com/unownone/server_config.git
@@ -31,5 +122,6 @@ git clone https://github.com/unownone/server_config.git
 # Change to the repository directory
 cd server_config
 
-# Run make
-make -i -s
+# Initialize the .env file
+# Get environment and execute init.sh script
+bash init.sh
