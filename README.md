17340d1fcf157ce1dca134fae9e704d7a2590cbb
Imon Roy <royimonroy@gmail.com>
2023-06-09 23:29:16 +0530

    Better management using make

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
 # server_config
-Server Config that is running on DigitalOcean
-## ENV REQUIRED
 
+Server Config that is running on DigitalOcean
+
+## ENV REQUIRED
 
 Create a .env file and put these in.
 
 # Set UP:
-- clone repo
-- set up env
-  - USER_EMAIL_ID = user's email id for letsencrypt
-  - DOMAIN = user's domain that is the main domain
-  - TRAEFIK_AUTH_PAIRS = Traefik auth pairs to be taken care of.
-  save this in .env file
-  
-- Set up a file called `acme.json`
-- change access to 600 ie `chmod 600 acme.json`
-- run server with `docker-compose up -d --build`
+
+- To set up just run `make` and it should guide you through it!
---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Ask for user input
+printf "Enter `USER_EMAIL_ID` to be used for LetsEncrypt \n"
+printf "Enter USER_EMAIL_ID: \n "
+read USER_EMAIL_ID
+printf "Enter DOMAIN to be used for routing. Make sure it is a top level domain. example `example.com` \n"
+printf "Enter DOMAIN: "
+read DOMAIN
+printf "Enter TRAEFIK_AUTH_PAIRS (Optional): "
+read TRAEFIK_AUTH_PAIRS
+
+# Create .env file with the values
+cat > .env << EOF
+USER_EMAIL_ID=${USER_EMAIL_ID}
+DOMAIN=${DOMAIN}
+TRAEFIK_AUTH_PAIRS=${TRAEFIK_AUTH_PAIRS}
+EOF
+
+printf ".env file created successfully\n"
+
+# Create acme.json file with correct permissions
+touch acme.json
+chmod 600 acme.json
+
+printf "acme.json file created successfully\n"
+
+printf "Your Portainer should be accesible at https://console.${DOMAIN}\n"
